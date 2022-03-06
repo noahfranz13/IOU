d40978d30ccc9849a19750a6351b90fe08c62af8
noahfranz13 <noahfranz13@gmail.com>
2022-03-05 19:39:43 -0500

    fix loadCSV file

--- DB/loadCSV_Sample.sql
+++ DB/loadCSV_Sample.sql
@@ -1,9 +1,9 @@
 SELECT *
-INTO OUTFILE ‘/tmp/patients.csv’
+INTO OUTFILE ‘/home/ubuntu/Documents/IOU/EVENT_TABLE.csv’
 FIELDS TERMINATED BY ‘,’
 ENCLOSED BY ‘”‘
 ESCAPED BY ‘\\’
 LINES TERMINATED BY ‘\n’
-FROM patients
+FROM EVENT_TABLE
 
 /*You can replace “*” with the join expression for your query.*/
