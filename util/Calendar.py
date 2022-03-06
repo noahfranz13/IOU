c44fc061a2d775b080eded2be26e0c418982a370
noahfranz13 <noahfranz13@gmail.com>
2022-03-06 09:10:21 -0500

    update Calendar

--- util/Calendar.py
+++ util/Calendar.py
@@ -9,61 +9,74 @@ from datetime import date, timedelta, datetime
 
 class Calendar:
 
-    def __init__(self, usernames):
+    def __init__(self, username):
 
-        if type(usernames) == str:
-            usernames = [usernames]
+        self.usernames = username
 
-        if len(usernames) > 3:
-            raise ValueError('Please only enter up to 3 usernames')
+    def plotEvents(self, today, defaultUserName=None):
+        '''
+        defaultUserName : the current user
+        '''
+        otherEvents = []
+        if defaultUserName != None:
 
-        self.usernames = usernames
+            query = f"""
+                    SELECT *
+                    FROM REQUESTS
+                    WHERE Requestor != '{defaultUserName}'
+                    """
 
-    def plotEvents(self, today):
+            reqTable = pd.read_sql(query, self.mysql)
+
+            for ii, row in reqTable.iterrows():
+                event = Event(row['Requestor'], row['EventName'], row['StartTime'], row['EndTime'], row['StartDate'])
+                otherEvents.append(event)
+
 
         colors = ['firebrick', 'dodgerblue', 'seagreen']
 
+        colorIdx = 0
+
         fig, axs = plt.subplots(1, 7, figsize=(30, 15))
 
-        for colorIdx, user in enumerate(self.usernames):
+        # get new ioObj
+        io = IO(self.usernames)
 
-            # get new ioObj
-            io = IO(user)
+        # generate list of next 7 days
+        datesList = [today + timedelta(days=i) for i in range(7)]
 
-            # generate list of next 7 days
-            datesList = [today + timedelta(days=i) for i in range(7)]
+        # generate plot of the users schedule for the next 7 days
+        font = {'family' : 'DejaVu Sans',
+                'weight' : 'bold',
+                'size' : 24}
+        rc('font', **font)
 
-            # generate plot of the users schedule for the next 7 days
-            font = {'family' : 'DejaVu Sans',
-                    'weight' : 'bold',
-                    'size' : 24}
-            rc('font', **font)
+        strTimes = [f"{ii}:00" for ii in range(24)]
+        axs[0].set_ylabel('Time [hh:mm]', fontsize=30)
 
-            strTimes = [f"{ii}:00" for ii in range(24)]
-            axs[0].set_ylabel('Time [hh:mm]', fontsize=30)
+        x = [0, 1]
 
-            x = [0, 1]
+        for ax, dd in zip(axs, datesList):
+            ax.set_title(dd.strftime("%m/%d"))
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_ylim(24)
 
-            for ax, dd in zip(axs, datesList):
-                ax.set_title(dd.strftime("%m/%d"))
-                ax.set_xticks([])
-                ax.set_yticks([])
-                ax.set_ylim(24)
+            for jj in range(24):
+                ax.axhline(jj, x[0], x[1], ls='--', color='k', alpha=0.5)
 
-                for jj in range(24):
-                    ax.axhline(jj, x[0], x[1], ls='--', color='k', alpha=0.5)
+            print(io.events+otherEvents)
+            for event in io.events+otherEvents:
+                if event.startTime.strftime("%m/%d") == dd.strftime("%m/%d"):
+                    startHr = int(event.startTime.strftime("%H"))
+                    startMin = int(event.startTime.strftime("%M"))
+                    endHr = int(event.endTime.strftime("%H"))
+                    endMin = int(event.endTime.strftime("%M"))
+                    ax.fill_between(x, startHr + startMin/60, endHr + endMin/60, color=colors[colorIdx], alpha=0.5)
+                    midpoint = (startHr + startMin/60 + endHr + endMin/60)/2
+                    ax.text(0, midpoint, event.eventName, color='w')
 
-                for event in io.events:
-                    if event.startTime.strftime("%m/%d") == dd.strftime("%m/%d"):
-                        startHr = int(event.startTime.strftime("%H"))
-                        startMin = int(event.startTime.strftime("%M"))
-                        endHr = int(event.endTime.strftime("%H"))
-                        endMin = int(event.endTime.strftime("%M"))
-                        ax.fill_between(x, startHr + startMin/60, endHr + endMin/60, color=colors[colorIdx], alpha=0.5)
-                        midpoint = (startHr + startMin/60 + endHr + endMin/60)/2
-                        ax.text(0, midpoint, event.eventName, color='w')
-
-            axs[0].set_yticks(np.arange(len(strTimes)), labels=strTimes)
-            fig.suptitle("Year: " + datesList[0].strftime("%Y"), fontsize=36)
+        axs[0].set_yticks(np.arange(len(strTimes)), labels=strTimes)
+        fig.suptitle("Year: " + datesList[0].strftime("%Y"), fontsize=36)
 
         return fig
